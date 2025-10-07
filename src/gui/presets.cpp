e509c028f14b49d1a1784ad9412b825cc58d32dd
Ashley LastName <noescapefrommonika@gmail.com>
2025-10-07 04:06:37 -0400

    get this trash out of my sight

--- src/gui/presets.cpp
+++ src/gui/presets.cpp
@@ -3424,122 +3424,122 @@ void FurnaceGUI::initSystemPresets() {
   CATEGORY_END;
 
   CATEGORY_BEGIN(_("FM"),_("chips which use frequency modulation (FM) to generate sound.\nsome of these also pack more (like square and sample channels)."));
-  ENTRY(
-    _("Yamaha YM2151 (OPM)"), {
-      CH(DIV_SYSTEM_YM2151, 1.0f, 0, "")
-    }
-  );
-  ENTRY(
-    _("Yamaha YM2203 (OPN)"), {
-      CH(DIV_SYSTEM_YM2203, 1.0f, 0, "clockSel=3")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YM2203 (extended channel 3)"), {
-        CH(DIV_SYSTEM_YM2203_EXT, 1.0f, 0, "clockSel=3")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2203 (CSM)"), {
-        CH(DIV_SYSTEM_YM2203_CSM, 1.0f, 0, "clockSel=3")
-      }
-    );
-  ENTRY(
-    _("Yamaha YM2608 (OPNA)"), {
-      CH(DIV_SYSTEM_YM2608, 1.0f, 0, "")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YM2608 (extended channel 3)"), {
-        CH(DIV_SYSTEM_YM2608_EXT, 1.0f, 0, "")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2608 (CSM)"), {
-        CH(DIV_SYSTEM_YM2608_CSM, 1.0f, 0, "")
-      }
-    );
-  ENTRY(
-    _("Yamaha YM2610 (OPNB)"), {
-      CH(DIV_SYSTEM_YM2610_FULL, 1.0f, 0, "")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YM2610 (extended channel 2)"), {
-        CH(DIV_SYSTEM_YM2610_FULL_EXT, 1.0f, 0, "")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2610 (CSM)"), {
-        CH(DIV_SYSTEM_YM2610_CSM, 1.0f, 0, "")
-      }
-    );
-  ENTRY(
-    _("Yamaha YM2610B (OPNB2)"), {
-      CH(DIV_SYSTEM_YM2610B, 1.0f, 0, "")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YM2610B (extended channel 3)"), {
-        CH(DIV_SYSTEM_YM2610B_EXT, 1.0f, 0, "")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2610B (CSM)"), {
-        CH(DIV_SYSTEM_YM2610B_CSM, 1.0f, 0, "")
-      }
-    );
-  ENTRY(
-    _("Yamaha YM2612 (OPN2)"), {
-      CH(DIV_SYSTEM_YM2612, 1.0f, 0, "chipType=1")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YM2612 (extended channel 3)"), {
-        CH(DIV_SYSTEM_YM2612_EXT, 1.0f, 0, "chipType=1")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2612 (OPN2) CSM"), {
-        CH(DIV_SYSTEM_YM2612_CSM, 1.0f, 0, "chipType=1")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2612 (OPN2) with DualPCM"), {
-        CH(DIV_SYSTEM_YM2612_DUALPCM, 1.0f, 0, "chipType=1")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM2612 (extended channel 3) with DualPCM"), {
-        CH(DIV_SYSTEM_YM2612_DUALPCM_EXT, 1.0f, 0, "chipType=1")
-      }
-    );
-  ENTRY(
-    _("Yamaha YMF276 (OPN2L)"), {
-      CH(DIV_SYSTEM_YM2612, 1.0f, 0, "chipType=2")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YMF276 (extended channel 3)"), {
-        CH(DIV_SYSTEM_YM2612_EXT, 1.0f, 0, "chipType=2")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YMF276 (OPN2L) CSM"), {
-        CH(DIV_SYSTEM_YM2612_CSM, 1.0f, 0, "chipType=2")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YMF276 (OPN2L) with DualPCM"), {
-        CH(DIV_SYSTEM_YM2612_DUALPCM, 1.0f, 0, "chipType=2")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YMF276 (extended channel 3) with DualPCM"), {
-        CH(DIV_SYSTEM_YM2612_DUALPCM_EXT, 1.0f, 0, "chipType=2")
-      }
-    );
-  ENTRY(
+  //ENTRY(
+  //  _("Yamaha YM2151 (OPM)"), {
+  //    CH(DIV_SYSTEM_YM2151, 1.0f, 0, "")
+  //  }
+  //);
+  //ENTRY(
+  //  _("Yamaha YM2203 (OPN)"), {
+  //    CH(DIV_SYSTEM_YM2203, 1.0f, 0, "clockSel=3")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2203 (extended channel 3)"), {
+  //      CH(DIV_SYSTEM_YM2203_EXT, 1.0f, 0, "clockSel=3")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2203 (CSM)"), {
+  //      CH(DIV_SYSTEM_YM2203_CSM, 1.0f, 0, "clockSel=3")
+  //    }
+  //  );
+  //ENTRY(
+  //  _("Yamaha YM2608 (OPNA)"), {
+  //    CH(DIV_SYSTEM_YM2608, 1.0f, 0, "")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2608 (extended channel 3)"), {
+  //      CH(DIV_SYSTEM_YM2608_EXT, 1.0f, 0, "")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2608 (CSM)"), {
+  //      CH(DIV_SYSTEM_YM2608_CSM, 1.0f, 0, "")
+  //    }
+  //  );
+  //ENTRY(
+  //  _("Yamaha YM2610 (OPNB)"), {
+  //    CH(DIV_SYSTEM_YM2610_FULL, 1.0f, 0, "")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2610 (extended channel 2)"), {
+  //      CH(DIV_SYSTEM_YM2610_FULL_EXT, 1.0f, 0, "")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2610 (CSM)"), {
+  //      CH(DIV_SYSTEM_YM2610_CSM, 1.0f, 0, "")
+  //    }
+  //  );
+  //ENTRY(
+  //  _("Yamaha YM2610B (OPNB2)"), {
+  //    CH(DIV_SYSTEM_YM2610B, 1.0f, 0, "")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2610B (extended channel 3)"), {
+  //      CH(DIV_SYSTEM_YM2610B_EXT, 1.0f, 0, "")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2610B (CSM)"), {
+  //      CH(DIV_SYSTEM_YM2610B_CSM, 1.0f, 0, "")
+  //    }
+  //  );
+  //ENTRY(
+  //  _("Yamaha YM2612 (OPN2)"), {
+  //    CH(DIV_SYSTEM_YM2612, 1.0f, 0, "chipType=1")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2612 (extended channel 3)"), {
+  //      CH(DIV_SYSTEM_YM2612_EXT, 1.0f, 0, "chipType=1")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2612 (OPN2) CSM"), {
+  //      CH(DIV_SYSTEM_YM2612_CSM, 1.0f, 0, "chipType=1")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2612 (OPN2) with DualPCM"), {
+  //      CH(DIV_SYSTEM_YM2612_DUALPCM, 1.0f, 0, "chipType=1")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM2612 (extended channel 3) with DualPCM"), {
+  //      CH(DIV_SYSTEM_YM2612_DUALPCM_EXT, 1.0f, 0, "chipType=1")
+  //    }
+  //  );
+  //ENTRY(
+  //  _("Yamaha YMF276 (OPN2L)"), {
+  //    CH(DIV_SYSTEM_YM2612, 1.0f, 0, "chipType=2")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YMF276 (extended channel 3)"), {
+  //      CH(DIV_SYSTEM_YM2612_EXT, 1.0f, 0, "chipType=2")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YMF276 (OPN2L) CSM"), {
+  //      CH(DIV_SYSTEM_YM2612_CSM, 1.0f, 0, "chipType=2")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YMF276 (OPN2L) with DualPCM"), {
+  //      CH(DIV_SYSTEM_YM2612_DUALPCM, 1.0f, 0, "chipType=2")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YMF276 (extended channel 3) with DualPCM"), {
+  //      CH(DIV_SYSTEM_YM2612_DUALPCM_EXT, 1.0f, 0, "chipType=2")
+  //    }
+  //  );
+  //ENTRY(
     _("Yamaha YM2413 (OPLL)"), {
       CH(DIV_SYSTEM_OPLL, 1.0f, 0, "")
     }
@@ -3554,31 +3554,31 @@ void FurnaceGUI::initSystemPresets() {
       CH(DIV_SYSTEM_OPZ, 1.0f, 0, "")
     }
   );
-  ENTRY(
-    _("Yamaha YM3438 (OPN2C)"), {
-      CH(DIV_SYSTEM_YM2612, 1.0f, 0, "chipType=0")
-    }
-  );
-    SUB_ENTRY(
-      _("Yamaha YM3438 (extended channel 3)"), {
-        CH(DIV_SYSTEM_YM2612_EXT, 1.0f, 0, "chipType=0")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM3438 (OPN2C) CSM"), {
-        CH(DIV_SYSTEM_YM2612_CSM, 1.0f, 0, "chipType=0")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM3438 (OPN2C) with DualPCM"), {
-        CH(DIV_SYSTEM_YM2612_DUALPCM, 1.0f, 0, "chipType=0")
-      }
-    );
-    SUB_ENTRY(
-      _("Yamaha YM3438 (extended channel 3) with DualPCM"), {
-        CH(DIV_SYSTEM_YM2612_DUALPCM_EXT, 1.0f, 0, "chipType=0")
-      }
-    );
+  //ENTRY(
+  //  _("Yamaha YM3438 (OPN2C)"), {
+  //    CH(DIV_SYSTEM_YM2612, 1.0f, 0, "chipType=0")
+  //  }
+  //);
+  //  SUB_ENTRY(
+  //    _("Yamaha YM3438 (extended channel 3)"), {
+  //      CH(DIV_SYSTEM_YM2612_EXT, 1.0f, 0, "chipType=0")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM3438 (OPN2C) CSM"), {
+  //      CH(DIV_SYSTEM_YM2612_CSM, 1.0f, 0, "chipType=0")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM3438 (OPN2C) with DualPCM"), {
+  //      CH(DIV_SYSTEM_YM2612_DUALPCM, 1.0f, 0, "chipType=0")
+  //    }
+  //  );
+  //  SUB_ENTRY(
+  //    _("Yamaha YM3438 (extended channel 3) with DualPCM"), {
+  //      CH(DIV_SYSTEM_YM2612_DUALPCM_EXT, 1.0f, 0, "chipType=0")
+  //    }
+  //  );
   ENTRY(
     _("Yamaha YM3526 (OPL)"), {
       CH(DIV_SYSTEM_OPL, 1.0f, 0, "")
